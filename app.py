56cb46ab869bb93033dd688e4c31bb248e25ef21
Tashi <tashishyangba23@gmail.com>
2024-07-07 22:00:11 -0400

    Add discord embed to style the job listing

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
+from discord.ext import commands, tasks
 
-
-from discord.ext import commands,tasks
-#discord env
+# discord env
 import discord
 import time
 import asyncio
 import os
 from dotenv import load_dotenv
 import json
+
 load_dotenv()
-token = os.environ.get('TOKEN')
+token = os.environ.get("TOKEN")
 import json
 import time
 
 client = discord.Client(intents=discord.Intents.default())
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.default())
 
 from backend.Linkedin import linkedin_jobs
 
@@ -23,11 +23,14 @@ def get_jobs(job_title):
     jobs = linkedin_jobs.get_jobs(job_title)
     return jobs
 
+
 def get_backend_jobs():
     return get_jobs("backend engineer")
 
+
 def get_frontend_jobs():
     return get_jobs("frontend engineer")
+
 
 def get_fullstack_jobs():
     return get_jobs("fullstack engineer")
@@ -35,146 +38,136 @@ def get_fullstack_jobs():
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print(f"Logged in as {bot.user}")
     send_all_jobs.start()
     clear_jobs.start()
-
-
-
-
 
 
 @tasks.loop(seconds=3600)
 async def send_backend_jobs():
     channel = bot.get_channel(1257135450133500055)
-    channel2 = bot.get_channel(1257137243483672608)
-    channel3 = bot.get_channel(1127641023741440101)
-    #build the message
-    #message = "```"
+    # channel2 = bot.get_channel(1257137243483672608)
+    # channel3 = bot.get_channel(1127641023741440101)
+    # build the message
+    # message = "```"
     jobs = linkedin_jobs.load_jobs("backend engineer")
     n = len(jobs)
-   
+
     for i in range(n):
-        jerb = f""" ``` ```
->>> ## Job Title: {jobs[i]['title']}
-**Company:** {jobs[i]['company']}
-**Location:** {jobs[i]['location']}
-**Link:** [Click here]({jobs[i]['job_url']})
-""" 
-        await channel.send(jerb)
-        await channel2.send(jerb)
-        await channel3.send(jerb)
+        embed = discord.Embed(
+            title=f"{jobs[i]['title']}",
+            description=f"**Company:** {jobs[i]['company']}\n**Location:** {jobs[i]['location']}\n**[Click here]({jobs[i]['job_url']})**",
+            color=discord.Color.blue(),
+        )
+
+        await channel.send(embed=embed)
+        # await channel2.send(jerb)
+        # await channel3.send(jerb)
 
 
 @tasks.loop(seconds=3800)
 async def send_frontend_jobs():
     channel = bot.get_channel(1257135450133500055)
-    channel2 = bot.get_channel(1257137243483672608)
-    channel3 = bot.get_channel(1127641023741440101)
-    #build the message
-    #message = "```"
+    # channel2 = bot.get_channel(1257137243483672608)
+    # channel3 = bot.get_channel(1127641023741440101)
+    # build the message
+    # message = "```"
     jobs = linkedin_jobs.load_jobs("frontend engineer")
     n = len(jobs)
 
     for i in range(n):
-        jerb = f""" ``` ```
->>> ## Job Title: {jobs[i]['title']}
-**Company:** {jobs[i]['company']}
-**Location:** {jobs[i]['location']}
-**Link:** [Click here]({jobs[i]['job_url']})
-""" 
-        await channel.send(jerb)
-        await channel2.send(jerb)
-        await channel3.send(jerb)
+        embed = discord.Embed(
+            title=f"{jobs[i]['title']}",
+            description=f"**Company:** {jobs[i]['company']}\n**Location:** {jobs[i]['location']}\n**[Click here]({jobs[i]['job_url']})**",
+            color=discord.Color.blue(),
+        )
 
-
+        await channel.send(embed=embed)
+        # await channel2.send(jerb)
+        # await channel3.send(jerb)
 
 
 @tasks.loop(seconds=4100)
 async def send_fullstack_jobs():
     channel = bot.get_channel(1257135450133500055)
-    channel2 = bot.get_channel(1257137243483672608)
-    channel3 = bot.get_channel(1127641023741440101)
-    #build the message
-    #message = "```"
+    # channel2 = bot.get_channel(1257137243483672608)
+    # channel3 = bot.get_channel(1127641023741440101)
+    # build the message
+    # message = "```"
     jobs = linkedin_jobs.load_jobs("fullstack engineer")
     n = len(jobs)
-     
-    for i in range(n):
-        jerb = f""" ``` ```
->>> ## Job Title: {jobs[i]['title']}
-**Company:** {jobs[i]['company']}
-**Location:** {jobs[i]['location']}
-**Link:** [Click here]({jobs[i]['job_url']})
-""" 
-        await channel.send(jerb)
-        await channel2.send(jerb)
-        await channel3.send(jerb)
 
+    for i in range(n):
+        embed = discord.Embed(
+            title=f"{jobs[i]['title']}",
+            description=f"**Company:** {jobs[i]['company']}\n**Location:** {jobs[i]['location']}\n**[Click here]({jobs[i]['job_url']})**",
+            color=discord.Color.blue(),
+        )
+
+        await channel.send(embed=embed)
+        # await channel2.send(jerb)
+        # await channel3.send(jerb)
 
 
 @tasks.loop(seconds=3600)
 async def send_all_jobs():
     """
-        send all jobs to channel, 
-        for each role wait 10 seconds before sending the next role
+    send all jobs to channel,
+    for each role wait 10 seconds before sending the next role
 
     """
     channel = bot.get_channel(1257135450133500055)
-    channel2 = bot.get_channel(1257137243483672608)
-    channel3 = bot.get_channel(1127641023741440101)
-    #build the message
-    #message = "```"
+    # channel2 = bot.get_channel(1257137243483672608)
+    # channel3 = bot.get_channel(1127641023741440101)
+    # build the message
+    # message = "```"
     jobs = linkedin_jobs.load_jobs("backend engineer")
     n = len(jobs)
-     
+
     for i in range(n):
-        jerb = f""" ``` ```
->>> ## Job Title: {jobs[i]['title']}
-**Company:** {jobs[i]['company']}
-**Location:** {jobs[i]['location']}
-**Link:** [Click here]({jobs[i]['job_url']})
-""" 
-        await channel.send(jerb)
-        await channel2.send(jerb)
-        await channel3.send(jerb)
+        embed = discord.Embed(
+            title=f"{jobs[i]['title']}",
+            description=f"**Company:** {jobs[i]['company']}\n**Location:** {jobs[i]['location']}\n**[Click here]({jobs[i]['job_url']})**",
+            color=discord.Color.blue(),
+        )
+
+        await channel.send(embed=embed)
+        # await channel2.send(jerb)
+        # await channel3.send(jerb)
     time.sleep(4)
     jobs = linkedin_jobs.load_jobs("frontend engineer")
     n = len(jobs)
-     
+
     for i in range(n):
-        jerb = f""" ``` ```
->>> ## Job Title: {jobs[i]['title']}
-**Company:** {jobs[i]['company']}
-**Location:** {jobs[i]['location']}
-**Link:** [Click here]({jobs[i]['job_url']})
-""" 
-        await channel.send(jerb)
-        await channel2.send(jerb)
-        await channel3.send(jerb)
+        embed = discord.Embed(
+            title=f"{jobs[i]['title']}",
+            description=f"**Company:** {jobs[i]['company']}\n**Location:** {jobs[i]['location']}\n**[Click here]({jobs[i]['job_url']})**",
+            color=discord.Color.blue(),
+        )
+
+        await channel.send(embed=embed)
+        # await channel2.send(jerb)
+        # await channel3.send(jerb)
     time.sleep(4)
     jobs = linkedin_jobs.load_jobs("fullstack engineer")
     n = len(jobs)
-     
+
     for i in range(n):
-        jerb = f""" ``` ```
->>> ## Job Title: {jobs[i]['title']}
-**Company:** {jobs[i]['company']}
-**Location:** {jobs[i]['location']}
-**Link:** [Click here]({jobs[i]['job_url']})
-""" 
-        await channel.send(jerb)
-        await channel2.send(jerb)
-        await channel3.send(jerb)
+        embed = discord.Embed(
+            title=f"{jobs[i]['title']}",
+            description=f"**Company:** {jobs[i]['company']}\n**Location:** {jobs[i]['location']}\n**[Click here]({jobs[i]['job_url']})**",
+            color=discord.Color.blue(),
+        )
+        await channel.send(embed=embed)
+        # await channel2.send(jerb)
+        # await channel3.send(jerb)
 
 
-#clear jobs after 24 hours
+# clear jobs after 24 hours
 @tasks.loop(seconds=86400)
 async def clear_jobs():
     print("CLEARING JOBS")
     linkedin_jobs.clear_jobs()
-
-
 
 
 bot.run(token)
